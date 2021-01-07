022f5088d67df4c52f86517db2ed0907c171ec37
swap795 <thapaswapnil08@gmail.com>
2021-01-07 00:31:52 -0500

    Changed symbols on the botomNavigation and created all of their initial pages

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -1,13 +1,24 @@
-import { Ionicons } from '@expo/vector-icons';
+import { Entypo, EvilIcons, MaterialCommunityIcons, FontAwesome5 } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
+
+// All of the screens
+import HomeScreen from '../screens/HomeScreen';
+import SearchScreen from '../screens/SearchScreen';
+import LibraryScreen from '../screens/LibraryScreen';
+import PremiumScreen from '../screens/PremiumScreen';
+
+import { 
+  BottomTabParamList, 
+  HomeParamList as HomeParamList, 
+  SearchParamList as SearchParamList,
+  LibraryParamList as LibraryParamList,
+  PremiumParamList as PremiumParamList
+} from '../types';
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
@@ -16,20 +27,34 @@ export default function BottomTabNavigator() {
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
+      initialRouteName="Home"
       tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
       <BottomTab.Screen
-        name="TabOne"
-        component={TabOneNavigator}
+        name="Home"
+        component={HomeNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ color }) => <Entypo name="home" size={ 30 } style={{ marginBottom: -3 }} color={ color } />,
         }}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoNavigator}
+        name="Search"
+        component={SearchNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ color }) => <EvilIcons name="search" size={ 30 } style={{ marginBottom: -3 }} color={ color } />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Library"
+        component={LibraryNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="playlist-music-outline" size={ 30 } style={{ marginBottom: -3 }} color={ color } />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Premium"
+        component={PremiumNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <FontAwesome5 name="spotify" size={ 30 } style={{ marginBottom: -3 }} color={ color } />,
         }}
       />
     </BottomTab.Navigator>
@@ -38,36 +63,64 @@ export default function BottomTabNavigator() {
 
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
-function TabBarIcon(props: { name: string; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
-}
+// function TabBarIcon(props: { name: string; color: string }) {
+//   return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+// }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
-const TabOneStack = createStackNavigator<TabOneParamList>();
+const HomeStack = createStackNavigator<HomeParamList>();
 
-function TabOneNavigator() {
+function HomeNavigator() {
   return (
-    <TabOneStack.Navigator>
-      <TabOneStack.Screen
-        name="TabOneScreen"
-        component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ headerTitle: 'Home Title' }}
       />
-    </TabOneStack.Navigator>
+    </HomeStack.Navigator>
   );
 }
 
-const TabTwoStack = createStackNavigator<TabTwoParamList>();
+const SearchStack = createStackNavigator<SearchParamList>();
 
-function TabTwoNavigator() {
+function SearchNavigator() {
   return (
-    <TabTwoStack.Navigator>
-      <TabTwoStack.Screen
-        name="TabTwoScreen"
-        component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
+    <SearchStack.Navigator>
+      <SearchStack.Screen
+        name="SearchScreen"
+        component={SearchScreen}
+        options={{ headerTitle: 'Search Title' }}
       />
-    </TabTwoStack.Navigator>
+    </SearchStack.Navigator>
+  );
+}
+
+const LibraryStack = createStackNavigator<LibraryParamList>();
+
+function LibraryNavigator() {
+  return (
+    <LibraryStack.Navigator>
+      <LibraryStack.Screen
+        name="LibraryScreen"
+        component={LibraryScreen}
+        options={{ headerTitle: 'Library Title' }}
+      />
+    </LibraryStack.Navigator>
+  );
+}
+
+const PremiumStack = createStackNavigator<PremiumParamList>();
+
+function PremiumNavigator() {
+  return (
+    <PremiumStack.Navigator>
+      <PremiumStack.Screen
+        name="PremiumScreen"
+        component={PremiumScreen}
+        options={{ headerTitle: 'Premium Title' }}
+      />
+    </PremiumStack.Navigator>
   );
 }
--- types.tsx
+++ types.tsx
@@ -4,14 +4,24 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Home: undefined;
+  Search: undefined;
+  Library: undefined;
+  Premium: undefined;
 };
 
-export type TabOneParamList = {
-  TabOneScreen: undefined;
+export type HomeParamList = {
+  HomeScreen: undefined;
 };
 
-export type TabTwoParamList = {
-  TabTwoScreen: undefined;
+export type SearchParamList = {
+  SearchScreen: undefined;
+};
+
+export type LibraryParamList = {
+  LibraryScreen: undefined;
+};
+
+export type PremiumParamList = {
+  PremiumScreen: undefined;
 };
